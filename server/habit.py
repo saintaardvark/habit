f2f0fc653ac2249be06a9f58f5b8eada6ceb63e4
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2022-09-04 13:38:01 -0700

    Add TODO about test data
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- server/habit.py
+++ server/habit.py
@@ -7,10 +7,11 @@ app = Flask(__name__)
 
 TEST_HABITS = ["veggie meal", "stretches", "cardio exercise"]
 
-for habit in TEST_HABITS:
-    new_entry = Habit(habitname=habit)
-    db.session.add(new_entry)
-    db.session.commit()
+# TODO: Only needed the once...
+# for habit in TEST_HABITS:
+#     new_entry = Habit(habitname=habit)
+#     db.session.add(new_entry)
+#     db.session.commit()
 
 
 @app.route("/")
@@ -27,8 +28,9 @@ def get_habit():
     Get habits
     """
     body = "<h1>Habits</h1><div id='habits'><ul>"
-    for habit in TEST_HABITS:
-        body += f"<li>{habit}</li>"
+    habits = Habit.query.all()
+    for habit in habits:
+        body += f"<li>{habit.habitname}</li>"
 
     body += "</ul>"
     return body
@@ -40,4 +42,10 @@ def create_habit(habit):
     Create habit
     """
     TEST_HABITS.append(habit)
+    new_entry = Habit(habitname=habit)
+    db.session.add(new_entry)
+    db.session.commit()
     return f"Added {habit}"
+
+if __name__ == '__main__':
+    db.app.run()
