cbdf21343bbeb1405702e12cc1e48b66c12c09cc
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2022-09-23 15:55:20 -0700

    Adjust calendar range based on whether mobile or not
    TODO:
    
    - I'd like the ability to toggle month vs year with a button.
    
    - I should refactor the setting of range, etc -- at the very least
    break it out to its own function; maybe (probably) have an argument
    for range ("month", "year") for showHeatMap().
    
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- server/static/js/script.js
+++ server/static/js/script.js
@@ -34,14 +34,22 @@ async function incrementHabit(id){
 };
 
 async function showHeatMap(id) {
+  let startTime = new Date();
+  startTime.setMonth(startTime.getMonth() - 6);
+  let domain = "month";
+  let subDomain = "day";
+  let range = 12;
+  if (/Mobi/.test(navigator.userAgent)) {
+    startTime = new Date();
+    range = 1;
+  }
   var cal = new CalHeatMap();
   cal.init({
-    start: new Date(),
-    range: 1,
-    domain: "month",
-    subDomain: "day",
+    start: startTime,
+    range: range,
+    domain: domain,
+    subDomain: subDomain,
     data: `/log/${id}`,
     itemSelector: `#cal-heatmap-${id}`
   });
-
 }
